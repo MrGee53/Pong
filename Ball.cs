ce0052ff215d930ae943f6e037beccad2be00b6f
Mr.Gee <tarker513@gmail.com>
2021-04-03 14:54:54 -0500

    Cleaned up code. Put the paddle and ball into their own files

---
+++ Ball.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace JankyPong
+{
+    public class Ball : Microsoft.Xna.Framework.DrawableGameComponent
+    {
+        
+        #region Private Members
+        private SpriteBatch spriteBatch;
+        private readonly ContentManager contentManager;
+
+        // Default speed of ball
+        private const float DEFAULT_X_SPEED = 150;
+        private const float DEFAULT_Y_SPEED = 150;
+
+        // Initial location of the ball
+        private const float INIT_X_POS = 80;
+        private const float INIT_Y_POS = 80;
+
+        private const float INCREASE_SPEED = 50;
+
+        // Ball sprite
+        private Texture2D ballSprite;
+
+        // Ball Location
+        private Vector2 ballPostition;
+
+        // Ball's Motion
+        Vector2 ballSpeed = new Vector2(DEFAULT_X_SPEED, DEFAULT_Y_SPEED);
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Gets or sets the Ball's horizontal speed.
+        /// </summary>
+        public float SpeedX
+        {
+            get { return ballSpeed.X; }
+            set { ballSpeed.X = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the Ball's vertical speed
+        /// </summary>
+        public float SpeedY
+        {
+            get { return ballSpeed.Y; }
+            set { ballSpeed.Y = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the X position of the ball.
+        /// </summary>
+        public float X
+        {
+            get { return ballPostition.X; }
+            set { ballPostition.X = value; }
+        }
+
+        /// <summary>
+        /// Gets or sets the Y position of the ball.
+        /// </summary>
+        public float Y
+        {
+            get { return ballPostition.Y; }
+            set { ballPostition.Y = value; }
+        }
+
+        /// <summary>
+        /// Gets the Width of the ball sprite
+        /// </summary>
+        public int Width
+        {
+            get { return ballSprite.Width; }
+        }
+
+        /// <summary>
+        /// Gets the Height of the ball sprite
+        /// </summary>
+        public int Height
+        {
+            get { return ballSprite.Height; }
+        }
+
+        /// <summary>
+        /// Gets the bounding rectangle of the ball.
+        /// </summary>
+        public Rectangle Boundary
+        {
+            get
+            {
+                return new Rectangle((int)ballPostition.X, (int)ballPostition.Y,
+                    ballSprite.Width, ballSprite.Height);
+            }
+        }
+        #endregion
+
+        public Ball(Game game)
+            : base(game)
+        {
+            contentManager = new ContentManager(game.Services);
+        }
+
+        /// <summary>
+        /// Set the ball in the middle of the screen with default
+        /// speed.
+        /// </summary>
+        public void Reset()
+        {
+            ballSpeed.X = DEFAULT_X_SPEED;
+            ballSpeed.Y = DEFAULT_Y_SPEED;
+
+            ballPostition.X = GraphicsDevice.Viewport.Width / 2 
+                - ballSprite.Width;
+            ballPostition.Y = GraphicsDevice.Viewport.Height / 2
+                - ballSprite.Height;
+        }
+
+        /// <summary>
+        /// Increases the ball's speed in both the X and Y
+        /// directions.
+        /// </summary>
+        public void SpeedUp()
+        {
+            if (ballSpeed.Y < 0)
+                ballSpeed.Y -= INCREASE_SPEED;
+            else
+                ballSpeed.Y += INCREASE_SPEED;
+
+            if (ballSpeed.X < 0)
+                ballSpeed.X -= INCREASE_SPEED;
+            else
+                ballSpeed.X += INCREASE_SPEED;
+        }
+
+        /// <summary>
+        /// Inverts the ball's horizontal direction.
+        /// </summary>
+        public void ChangeHorzDirection()
+        {
+            ballSpeed.X *= -1;
+        }
+
+        /// <summary>
+        /// Inverts the ball's vertical direction
+        /// </summary>
+        public void ChangeVertDirection()
+        {
+            ballSpeed.Y *= -1;
+        }
+
+        /// <summary>
+        /// Allows the game component to perform any initialization it needs
+        /// before starting to run
+        /// </summary>
+        public override void Initialize()
+        {
+            ballPostition.X = INIT_X_POS;
+            ballPostition.Y = INIT_Y_POS;
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place
+        /// to load all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            ballSprite = contentManager.Load<Texture2D>(@"Content\testball");
+
+            ballPostition.X = GraphicsDevice.Viewport.Width / 2
+                - ballSprite.Width;
+            ballPostition.Y = GraphicsDevice.Viewport.Height / 2
+                - ballSprite.Height;
+        }
+
+        /// <summary>
+        /// Allows the game component to update itself.
+        /// </summary>
+        /// <param name="gameTime">
+        /// Provides a snapshot of timing values.
+        /// </param>
+        public override void Update(GameTime gameTime)
+        {
+            // Move the sprite by speed, scaled by elapsed time.
+            ballPostition += ballSpeed * 
+                (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        public override void Draw(GameTime gameTime)
+        {
+            base.Draw(gameTime);
+
+            spriteBatch.Begin();
+            spriteBatch.Draw(ballSprite, ballPostition, Color.White);
+            spriteBatch.End();
+        }
+    }
+}
--- Game1.cs
+++ Game1.cs
@@ -9,26 +9,20 @@ namespace JankyPong
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
-        // Ball sprite
-        Texture2D ballSprite;
-
-        // Ball location
-        Vector2 ballPos;
-
-        // Information about the sprites movement
-        Vector2 ballSpeed = new Vector2(150, 150);
-
-        // Paddle sprite
-        Texture2D paddleSprite;
-
-        // Paddle location
-        Vector2 paddlePos;
+        private Ball ball;
+        private Paddle paddle;
 
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+
+            paddle = new Paddle(this);
+            ball = new Ball(this);
+
+            Components.Add(paddle);
+            Components.Add(ball);
         }
 
         protected override void Initialize()
@@ -43,15 +37,6 @@ namespace JankyPong
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
-
-            ballSprite = Content.Load<Texture2D>("testBall");
-
-            paddleSprite = Content.Load<Texture2D>("testPaddle");
-
-            ballPos = new Vector2(
-                GraphicsDevice.Viewport.Width / 2 - ballSprite.Width / 2 ,
-                GraphicsDevice.Viewport.Height / 2 - ballSprite.Height / 2
-                );
         }
 
         protected override void Update(GameTime gameTime)
@@ -60,62 +45,28 @@ namespace JankyPong
                 Exit();
 
             // TODO: Add your update logic here
-
-            // Move the Sprite by speed multiplied by elapsed time
-            ballPos += ballSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
-
-            int maxX = GraphicsDevice.Viewport.Width - ballSprite.Width;
-            int maxY = GraphicsDevice.Viewport.Height - ballSprite.Height;
+            int maxX = GraphicsDevice.Viewport.Width;
+            int maxY = GraphicsDevice.Viewport.Height;
 
             // Check if ball is hitting a wall
-            if (ballPos.X > maxX)
-                ballSpeed.X *= -1;
+            if (ball.X > maxX - ball.Width)
+                ball.ChangeHorzDirection();
+            else if (ball.X < 0)
+                ball.Reset();
 
-            if (ballPos.X < 0)
+            if (ball.Y > maxY - ball.Height)
+                ball.ChangeVertDirection();
+            else if (ball.Y < 0)
+                ball.ChangeVertDirection();
+
+
+            // Check if the ball is hitting the paddle
+            if (ball.Boundary.Intersects(paddle.Boundary) && ball.SpeedX < 0)
             {
-                ballPos.X = maxX / 2;
-                ballPos.Y = maxY / 2;
-                ballSpeed.X = 150;
-                ballSpeed.Y = 150;
+                ball.SpeedUp();
+
+                ball.ChangeHorzDirection();
             }
-             
-            if (ballPos.Y > maxY || ballPos.Y < 0)
-                ballSpeed.Y *= -1;
-            
-            // Check if the ball hit the paddle
-            Rectangle ballRect =
-                new Rectangle((int)ballPos.X, (int)ballPos.Y,
-                paddleSprite.Width, paddleSprite.Height);
-
-            Rectangle paddleRect =
-                new Rectangle((int)paddlePos.X, (int)paddlePos.Y,
-                paddleSprite.Width, paddleSprite.Height);
-
-            if (ballRect.Intersects(paddleRect) && ballSpeed.X < 0)
-            {
-                // Increase speed
-                ballSpeed.Y += 50;
-                if (ballSpeed.X < 0)
-                    ballSpeed.X -= 50;
-                else
-                    ballSpeed.X += 50;
-
-                // Send ball back up the screen
-                ballSpeed.X *= -1;
-            }
-
-            // Keep the paddle on screen
-            if (paddlePos.Y < 0)
-                paddlePos.Y = 0;
-            if (paddlePos.Y > maxY - paddleSprite.Height / 2)
-                paddlePos.Y = maxY - paddleSprite.Height /2;
-
-            // Check for Input
-            KeyboardState keyState = Keyboard.GetState();
-            if (keyState.IsKeyDown(Keys.Down))
-                paddlePos.Y += 5;
-            else if (keyState.IsKeyDown(Keys.Up))
-                paddlePos.Y -= 5;
 
             base.Update(gameTime);
         }
@@ -126,10 +77,6 @@ namespace JankyPong
 
             // TODO: Add your drawing code here
 
-            _spriteBatch.Begin();
-            _spriteBatch.Draw(ballSprite, ballPos, Color.White);
-            _spriteBatch.Draw(paddleSprite, paddlePos, Color.White);
-            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
